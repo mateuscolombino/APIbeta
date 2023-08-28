a1a54b864b306a0ccb8816486f9443b387024464
Mateus Colombino <syde4k@gmail.com>
2023-08-28 12:00:25 -0300

    Versão Beta API

---
+++ API/CodePulse.API/CodePulse.API/Data/AplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CodePulse.API.Models.Domain;
+namespace CodePulse.API.Data
+{
+    public class AplicationDbContext : DbContext
+    {
+        public AplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
